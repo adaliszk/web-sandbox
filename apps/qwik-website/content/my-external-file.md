18b6a388255a49ce7b9ea823029c5091dd44c239
Ádám Liszkai <adam@liszkai.dev>
2024-04-28 08:28:43 +0000

    feat(qwik-website): ✨ add MVP for loading markdown files
    With this, the concept of loading Markdown or MDX files are validated, and the experiment checks can be written.

---
+++ apps/qwik-website/content/my-external-file.md
@@ -0,0 +1,5 @@
+---
+title: Example external Markdown file
+---
+
+This is an example of an external Markdown file that can be included in the main content.
--- apps/qwik-website/src/routes/index.tsx
+++ apps/qwik-website/src/routes/index.tsx
@@ -1,20 +1,44 @@
 import type { DocumentHead } from "@builder.io/qwik-city";
-import { Link } from "@builder.io/qwik-city";
+import { routeLoader$, Link } from "@builder.io/qwik-city";
+import { parseMarkdown } from "../helpers/markdownParser";
 import { component$ } from "@builder.io/qwik";
 
 export const head: DocumentHead = {
-    title: "Qwik ISR Prototype"
+    title: "Qwik ISR Prototype",
 };
 
+export const useMarkdownFile = routeLoader$(async () => {
+    const data = await parseMarkdown({ file: "my-external-file" });
+    return {
+        frontmatter: data.frontmatter,
+        content: data.content,
+    };
+})
+
 export default component$(() => {
+    const markdown = useMarkdownFile();
+
     return (
         <>
             <h1>ISG Prototype</h1>
+            <p>Markdown contents:</p>
+            <pre>Frontmatter: {JSON.stringify(markdown.value.frontmatter, null, 2)}</pre>
+            {markdown.value.content}
+
             <p>Available pages:</p>
             <ul>
-                <li> <Link href={'/foo'}>/foo</Link> </li>
-                <li> <Link href={'/bar'}>/bar</Link> </li>
-                <li> <Link href={'/qux'}>/qux</Link> </li>
+                <li>
+                    {" "}
+                    <Link href={"/foo"}>/foo</Link>{" "}
+                </li>
+                <li>
+                    {" "}
+                    <Link href={"/bar"}>/bar</Link>{" "}
+                </li>
+                <li>
+                    {" "}
+                    <Link href={"/qux"}>/qux</Link>{" "}
+                </li>
             </ul>
         </>
     );
